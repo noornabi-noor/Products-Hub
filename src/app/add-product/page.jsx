ee2ea263d431f3039b84ffad4f0cbbd7e56004de
Noornabi <noornabimd9891@gmail.com>
2025-08-22 17:04:34 +0600

    finally finished

--- src/app/add-product/page.jsx
+++ src/app/add-product/page.jsx
@@ -1,10 +1,40 @@
-// app/add-product/page.tsx
+// // app/add-product/page.tsx
+// import ProductForm from "@/components/ProductForm";
+
+// export default function AddProductPage() {
+//   return (
+//     <div className="min-h-screen p-8 bg-gray-50">
+//       <ProductForm />
+//     </div>
+//   );
+// }
+
+
+"use client";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import toast from "react-hot-toast";
 import ProductForm from "@/components/ProductForm";
 
 export default function AddProductPage() {
-  return (
-    <div className="min-h-screen p-8 bg-gray-50">
-      <ProductForm />
-    </div>
-  );
+  const { data: session, status } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (status === "unauthenticated") {
+      toast.error("Please login to add products");
+      router.push("/login");
+    }
+  }, [status, router]);
+
+  if (status === "loading") {
+    return (
+      <div className="flex justify-center items-center min-h-screen">
+        <p className="text-lg font-semibold">Checking authentication...</p>
+      </div>
+    );
+  }
+
+  return <ProductForm />;
 }
--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -11,20 +11,13 @@ const handler = NextAuth({
   pages: {
     signIn: "/login",
   },
-  // callbacks: {
-  //   async redirect({ url, baseUrl }) {
-  //     return "/products"; 
-  //   },
-  // },
 
-  callbacks: {
+callbacks: {
   async redirect({ url, baseUrl }) {
-    // If the URL is on the same origin, return it
-    if (url.startsWith(baseUrl)) return url;
-    // Otherwise, fallback to homepage
-    return baseUrl;
+    return baseUrl; 
   },
 },
+
 });
 
 export { handler as GET, handler as POST };
